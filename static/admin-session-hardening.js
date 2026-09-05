380039ba31a55bfe242522fded5260492f4709f7
yarobkl <eliebakala@gmail.com>
2026-09-05 13:22:53 -0700

    feat: load case treatment integrity module

--- static/admin-session-hardening.js
+++ static/admin-session-hardening.js
@@ -27,6 +27,7 @@
     ['data-bininga-admin-core', '/static/admin-core.js?v=20260905-admin-boot-1'],
     ['data-bininga-admin-navigation', '/static/admin-navigation.js?v=20260905-admin-perf-2'],
     ['data-bininga-admin-integrity', '/static/admin-hardening.js?v=20260819-integrity-1'],
+    ['data-bininga-case-treatment', '/static/admin-case-treatment.js?v=20260905-case-flow-1'],
     ['data-bininga-dashboard-priority', '/static/admin-dashboard-priority.js?v=20260905-admin-boot-1'],
   ];
 
